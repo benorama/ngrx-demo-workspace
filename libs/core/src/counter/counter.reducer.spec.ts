b46abd9a9847d74a776ee3f494c68bc2174b3931
Benoit Hediard <ben@agorapulse.com>
2018-01-09 10:18:04 +0100

    Minor code cleaning

--- libs/core/src/counter/counter.reducer.spec.ts
+++ libs/core/src/counter/counter.reducer.spec.ts
@@ -1,11 +1,11 @@
 import {counterReducer} from './counter.reducer';
 import {CounterState, counterInitialState} from './counter.state';
-import {IncrementAction} from './counter.actions';
+import {CounterActions} from './counter.actions';
 
 describe('counterReducer', () => {
     it('should increment', () => {
         const state: CounterState = counterInitialState;
-        const action: IncrementAction = new IncrementAction();
+        const action: CounterActions.IncrementAction = new CounterActions.IncrementAction();
         const actual = counterReducer(state, action);
         expect(actual).toEqual({total: 1});
     });
