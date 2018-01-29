7b38fcec8de053a5715bde90b718d0093a1ac15f
Eden Toledano <edentoledano@gmail.com>
2018-01-30 00:13:18 +0200

    Simple fix for using scss file in a code-module component + usage example.

--- libs/core/src/my-component/my-component.component.ts
+++ libs/core/src/my-component/my-component.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-my-component',
-    templateUrl: './my-component.component.html'
+    templateUrl: './my-component.component.html',
+    styleUrls: ['/my-component.component.scss']
 })
 export class MyComponentComponent implements OnInit {
     constructor() {
