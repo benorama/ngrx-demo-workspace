69c7d15fbacc9308a80d16b10b5b4cc06e5fd54a
Benoit Hediard <ben@agorapulse.com>
2018-03-01 14:43:34 +0100

    Minor fix

--- libs/core/src/my-component/my-component.component.ts
+++ libs/core/src/my-component/my-component.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-my-component',
     templateUrl: './my-component.component.html',
-    styleUrls: ['/my-component.component.scss']
+    styleUrls: ['./my-component.component.scss']
 })
 export class MyComponentComponent implements OnInit {
     constructor() {
