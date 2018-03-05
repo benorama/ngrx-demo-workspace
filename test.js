13fc67a50e8cf8744adf9c4d12c679187773d92a
Benoit Hediard <ben@agorapulse.com>
2018-03-05 12:19:08 +0100

    Lib upgrade: Angular 5.2.7

--- test.js
+++ test.js
@@ -1,22 +1,17 @@
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-require('zone.js/dist/long-stack-trace-zone');
-require('zone.js/dist/proxy.js');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/jasmine-patch');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
-const getTestBed = require('@angular/core/testing').getTestBed;
-const BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
-const platformBrowserDynamicTesting = require('@angular/platform-browser-dynamic/testing').platformBrowserDynamicTesting;
+require('zone.js/dist/zone-testing');
+const getTestBed  = require('@angular/core/testing').getTestBed;
+const BrowserDynamicTestingModule  = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
+const platformBrowserDynamicTesting  = require('@angular/platform-browser-dynamic/testing').platformBrowserDynamicTesting;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {
-};
+__karma__.loaded = function () {};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
 const contextApps = require.context('./apps', true, /\.spec\.ts$/);
@@ -28,4 +23,5 @@ const contextLibs = require.context('./libs', true, /\.spec\.ts$/);
 contextLibs.keys().map(contextLibs);
 
 // Finally, start Karma to run the tests.
-__karma__.start();
+__karma__.start();    
+    
